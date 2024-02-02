75e6be9987a0731daf80c357f4a0d5e50358bdf3
Salomon Chambi <salomonchambi@gmail.com>
2024-02-02 09:00:00 -0500

    refactor: just use Component when definig a class

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import React, { Component } from 'react';
 import Table from './Table';
 
-class App extends React.Component {
+// class App extends React.Component { // works the same as below
+class App extends Component {
   render() {
     return (
       <div className="App">
