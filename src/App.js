e44c71d5e66fcf77aeaf9d21ff5c2ac8c58c1ca3
Salomon Chambi <salomonchambi@gmail.com>
2024-02-03 09:00:00 -0500

    feat: move table body data in js object format

--- src/App.js
+++ src/App.js
@@ -5,9 +5,28 @@ import Table from './Table';
 // class App extends React.Component { // works the same as below
 class App extends Component {
   render() {
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ];
+
     return (
-      <div className="App">
-        <Table />
+      <div className="container">
+        <Table characterData={characters} />
         {/* <h1>Hello, React!</h1> */}
       </div>
     )
