ab3ae3b317abf4f13352facb035b8b02a9a8fb7b
Salomon Chambi <salomonchambi@gmail.com>
2024-02-06 09:00:00 -0500

    feat: create Form class component and change handler function

---
+++ src/Form.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  initialState = {
+    name: '',
+    job: '',
+  }
+
+  state = this.initialState
+
+  handleChange = (event) => {
+    const { name, value } = event.target
+  
+    this.setState({
+      [name]: value,
+    })
+  }
+}
+
+export default Form;
